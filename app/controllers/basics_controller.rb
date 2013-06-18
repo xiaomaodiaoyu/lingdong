1617d6eea4875f169a4e3d43e5339dc6cc9fa80a
zhj <zhj.via@gmail.com>
2013-06-18 21:38:09 +0800

    first version copy

--- app/controllers/basics_controller.rb
+++ app/controllers/basics_controller.rb
@@ -1,5 +1,6 @@
 class BasicsController < ApplicationController
   def home
+=begin    
   	@headline = Faker::LoremCN.word
   	@subline = Faker::LoremCN.words.join
   	@first_point = Faker::LoremCN.words.join
@@ -8,15 +9,15 @@ class BasicsController < ApplicationController
   	@second_description = Faker::LoremCN.paragraph
   	@third_point = Faker::LoremCN.words.join
   	@third_description = Faker::LoremCN.paragraph
-=begin
-    @headline = t :headline 
-    @subline = t :subline
-    @first_point = t(:first_point, COMPANY_NAME: COMPANY_NAME)
-    @first_description = t(:first_description, COMPANY_NAME: COMPANY_NAME)
-    @second_point = t(:second_point, COMPANY_NAME: COMPANY_NAME)
-    @second_description = t(:second_description, COMPANY_NAME: COMPANY_NAME)
-    @third_point = t(:third_point, COMPANY_NAME: COMPANY_NAME)
-    @third_description = t(:third_description, COMPANY_NAME: COMPANY_NAME)
-=end    
+=end
+    @headline = t :headline
+    @subline = Faker::LoremCN.words.join 
+    #@subline = t :subline
+    @first_point = t(:first_point)
+    @first_description = t(:first_description)
+    @second_point = t(:second_point)
+    @second_description = t(:second_description)
+    @third_point = t(:third_point)
+    @third_description = t(:third_description)  
   end	
 end
